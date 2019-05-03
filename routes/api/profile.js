0f1046d934c85205c000c8b042d20c5e7bf93ff7
gilad bergmann <giladberg@gmail.com>
2019-05-03 13:47:06 +0300

    qq

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
 
-// @route    GET api/profile
-// @desc     Test  route
-// @access   Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route    GET api/profile/me
+// @desc     Get current users profile
+// @access   private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    s;
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    POST api/profile
+// @desc     Create or update users profile
+// @access   private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    //Build profuke object
+    const profileField = {};
+    profileField.user = req.user.id;
+    if (company) profileField.company = company;
+    if (website) profileField.company = company;
+    if (company) profileField.company = company;
+    if (company) profileField.company = company;
+    if (company) profileField.company = company;
+    if (company) profileField.company = company;
+  }
+);
 
 module.exports = router;
