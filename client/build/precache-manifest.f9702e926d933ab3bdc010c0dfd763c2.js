cb83e524f5a01049e999a67233e43b6a436bcdc0
gilad bergmann <giladberg@gmail.com>
2019-05-25 18:12:59 +0300

    prepared for deployment

---
+++ client/build/precache-manifest.f9702e926d933ab3bdc010c0dfd763c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f4e7b36f6ab5ec165678d1c8e569dd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3db766e8d74f9cf0a4",
+    "url": "/static/css/main.11b62bc3.chunk.css"
+  },
+  {
+    "revision": "158b89bc2101a350880c",
+    "url": "/static/js/2.416fb562.chunk.js"
+  },
+  {
+    "revision": "fa3db766e8d74f9cf0a4",
+    "url": "/static/js/main.d865f605.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
